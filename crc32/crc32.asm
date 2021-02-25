5fde09559e2e6dcbc2e81a8c65f64c15052f8266
Baek Kwang-ho <bkwgho@yandex.com>
2021-02-25 21:07:31 +0100

    added a couple of comments

--- crc32/crc32.asm
+++ crc32/crc32.asm
@@ -1,5 +1,8 @@
 ; This file computes the CRC32 of string given as argument
-; using the inverse polynomial
+; using the inverse polynomial.
+; It was extracted from a malware.
+
+; Note that the nullbyte of the string is taken into account !
 
 ; bkwg@hola.re
 
@@ -16,53 +19,63 @@ _start:
 cmp DWORD [esp], 2
 jnz exit
 
+; Beginning of CRC32 calculation
+
+; The real CRC is in dx:cx
+; But the code is using a tmp var, bx:ax, for computation
+
 loc_3301:
-                mov edi, DWORD [esp + 8]
-                mov ebx, edi
-                mov esi, edi
+                mov     edi, DWORD [esp + 8]
+                mov     ebx, edi
+                mov     esi, edi
                 xor     al, al
 
 loc_330D:
-                scasb   ; computing function name length
+                scasb                   ; computing function name length
                 jnz     loc_330D
 
                 sub     edi, ebx
                 cld
-                mov     ecx, -1
-                mov     edx, ecx
+                mov     ecx, -1         ; 1) Initializing CRC
+                mov     edx, ecx        ;         //
 
 loc_331A:
                 xor     eax, eax
                 xor     ebx, ebx
-                lodsb                   ; load first byte of function name
-                xor     al, cl
-                mov     cl, ch          ; shrd dx, cx, 8
-                                        ; mov dh, 8
-                mov     ch, dl
-                mov     dl, dh
+                lodsb                   ; 1.2) Load first byte of function name
+
+                xor     al, cl          ; 2) XORing nth byte with current CRC
+
+                mov     cl, ch          ; making things complicated here:
+                mov     ch, dl          ; the code is using al to save the lowest byte
+                mov     dl, dh          ; in order to use dh as a counter (see below)
                 mov     dh, 8
 
 loc_3329:
                 shr     bx, 1
-                rcr     ax, 1
-                jnb     loc_333A
-                xor     ax, 8320h       ; CRC-32 (using inverse polynomial : 0xEDB88320)
-                xor     bx, 0EDB8h
+                rcr     ax, 1           ; 3) odd ?
+                jnb     loc_333A        ; 3.1) if not, just leave the crc shifted
+                xor     ax, 8320h       ; 3.2) if yes xor with the inversed polynomial 0xEDB88320)
+                xor     bx, 0EDB8h      ;      //
 
 loc_333A:
-                dec     dh
+                dec     dh              ; 3.3) for each bit repeat the preceding process
                 jnz     loc_3329
                 xor     ecx, eax
                 xor     edx, ebx
-                dec     edi
+                dec     edi             ; dec length
                 jnz     loc_331A
                 not     edx
                 not     ecx
+
+                ; combining results
                 mov     eax, edx
                 rol     eax, 10h
                 mov     ax, cx
-                ; result in eax
 
+                ; final result in eax
+
+; print result in hex
 print_hex:
     mov ecx, 8
     rotate:
@@ -86,5 +99,5 @@ print_hex:
 
 exit:
     mov eax, 1
-    mov ebx, 0 
+    mov ebx, 0
     int 0x80
